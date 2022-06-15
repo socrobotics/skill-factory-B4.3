939c61951f0c8172328d84d8990dd532ea7e7dda
denismakarov <socrobotic@gmail.com>
2022-06-15 12:59:05 +0300

    change

--- main.py
+++ main.py
@@ -1,55 +1,60 @@
-board = list(range(1,10))
+dashboard = list(range(1, 10))
 
-def draw_board(board):
-    print ("-" * 13)
+
+def draw_board(dashboard):
+    print("-" * 13)
     for i in range(3):
-        print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-        print ("-" * 13)
+        print("|", dashboard[0 + i * 3], "|", dashboard[1 + i * 3], "|", dashboard[2 + i * 3], "|")
+        print("-" * 13)
 
-def take_input(player_token):
+
+def take_input(player):
     valid = False
     while not valid:
-        player_answer = input("Куда поставим " + player_token+"? ")
+        player_answer = input("Куда поставим " + player + "? ")
         try:
             player_answer = int(player_answer)
         except:
-            print ("Некорректный ввод. Вы уверены, что ввели число?")
+            print("Некорректный ввод. Вы уверены, что ввели число?")
             continue
         if player_answer >= 1 and player_answer <= 9:
-            if (str(board[player_answer-1]) not in "XO"):
-                board[player_answer-1] = player_token
+            if (str(dashboard[player_answer - 1]) not in "XO"):
+                dashboard[player_answer - 1] = player
                 valid = True
             else:
-                print ("Эта клеточка уже занята")
+                print(f"В данной клетке уже стоит {str(dashboard[player_answer - 1])}")
         else:
-            print ("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+            print("Некорректный ввод. Введите число от 1 до 9 чтобы продолжить.")
 
-def check_win(board):
-    win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+
+def check_win(dashboard):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
     for each in win_coord:
-        if board[each[0]] == board[each[1]] == board[each[2]]:
-            return board[each[0]]
+        if dashboard[each[0]] == dashboard[each[1]] == dashboard[each[2]]:
+            return dashboard[each[0]]
     return False
 
-def main(board):
+
+def main(dashboard):
     counter = 0
     win = False
     while not win:
-        draw_board(board)
+        draw_board(dashboard)
         if counter % 2 == 0:
             take_input("X")
         else:
             take_input("O")
         counter += 1
         if counter > 4:
-            tmp = check_win(board)
+            tmp = check_win(dashboard)
             if tmp:
-                print (tmp, "выиграл!")
+                print(tmp, "выиграл!")
                 win = True
                 break
         if counter == 9:
-            print ("Ничья!")
+            print("Ничья!")
             break
-    draw_board(board)
+    draw_board(dashboard)
 
-main(board)
+
+main(dashboard)
