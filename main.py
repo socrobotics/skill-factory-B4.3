33c8e28b387f3f700d875784550bec831757450a
denismakarov <socrobotic@gmail.com>
2022-06-15 14:35:18 +0300

    change

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def take_input(player):
 
 def check_win(dashboard):
     win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    for each in win_coord:
-        if dashboard[each[0]] == dashboard[each[1]] == dashboard[each[2]]:
-            return dashboard[each[0]]
+    for item in win_coord:
+        if dashboard[item[0]] == dashboard[item[1]] == dashboard[item[2]]:
+            return dashboard[item[0]]
     return False
 
 
